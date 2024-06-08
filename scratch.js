dcc97b518c82535bc0e2e7f62c4fc1c935a648f0
Andrew Emeghebo <andrewemeghebo2023@gmail.com>
2024-06-08 14:05:15 -0400

    First Commit

---
+++ scratch.js
@@ -0,0 +1,13 @@
+// "use strict";
+// const persons = {
+//   firstProfile: {
+//     name: "andrew",
+//   },
+//   secondProfile: {
+//     name: "bayo",
+//   },
+// };
+
+// for (const item of Object.entries(persons)) {
+//   console.log(item);
+// }
